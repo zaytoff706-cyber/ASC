ef3c529ba4be7aa5f1488ec12f9edf2f9f26a2af
zaytoff706-cyber <zaytoff706@gmail.com>
2026-08-21 21:05:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -11,4 +11,3 @@ LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID") or "0")
 VERIFIED_ROLE_ID = int(os.getenv("VERIFIED_ROLE_ID") or "0")
 
 COOLDOWN_SECONDS = int(os.getenv("COOLDOWN_SECONDS") or "300")
-PORT = int(os.getenv("PORT") or "8080")
