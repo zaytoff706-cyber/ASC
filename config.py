a4b2ef72757bddbd07b8c3bf7f2477f3aa9ec0c6
zaytoff706-cyber <zaytoff706@gmail.com>
2026-08-21 20:50:06 +0200

    Refactor environment variable loading in config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-﻿import os
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-GUILD_ID = int(os.getenv("GUILD_ID", "0"))
-STAFF_GUILD_ID = int(os.getenv("STAFF_GUILD_ID", "0"))
-STAFF_CHANNEL_ID = int(os.getenv("STAFF_CHANNEL_ID", "0"))
-LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", "0"))
-VERIFIED_ROLE_ID = int(os.getenv("VERIFIED_ROLE_ID", "0"))
+GUILD_ID = int(os.getenv("GUILD_ID") or "0")
+STAFF_GUILD_ID = int(os.getenv("STAFF_GUILD_ID") or "0")
+STAFF_CHANNEL_ID = int(os.getenv("STAFF_CHANNEL_ID") or "0")
+LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID") or "0")
+VERIFIED_ROLE_ID = int(os.getenv("VERIFIED_ROLE_ID") or "0")
 
-COOLDOWN_SECONDS = int(os.getenv("COOLDOWN_SECONDS", "300"))
-PORT = int(os.getenv("PORT", "8080"))
+COOLDOWN_SECONDS = int(os.getenv("COOLDOWN_SECONDS") or "300")
+PORT = int(os.getenv("PORT") or "8080")
