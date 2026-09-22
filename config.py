5830642359f0901d492bc2f018bb5595e0869f7d
zaytoff706-cyber <zaytoff706@gmail.com>
2026-09-22 09:36:52 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GUILD_ID = int(os.getenv("GUILD_ID") or "0")
+STAFF_GUILD_ID = int(os.getenv("STAFF_GUILD_ID") or "0")
+STAFF_CHANNEL_ID = int(os.getenv("STAFF_CHANNEL_ID") or "0")
+LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID") or "0")
+VALIDATION_CHANNEL_ID = int(os.getenv("VALIDATION_CHANNEL_ID") or "0")
+PROOF_CHANNEL_ID = int(os.getenv("PROOF_CHANNEL_ID") or "0")
+VERIFIED_ROLE_ID = int(os.getenv("VERIFIED_ROLE_ID") or "0")
+COOLDOWN_SECONDS = int(os.getenv("COOLDOWN_SECONDS") or "300")
+PORT = int(os.getenv("PORT") or "10000")
